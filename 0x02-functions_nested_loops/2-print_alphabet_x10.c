d8e434e91494b1363b0423115aab3b36787be51b
med-doba <mac@MacBook-Pro-de-mac-2.local>
2023-02-22 00:23:55 +0100

    10 times alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase + '\n'.
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int	i, j;
+
+	j = 0;
+	while (j < 10)
+	{
+		i = 0;
+		while (i < 26)
+			_putchar(97 + i++);
+		j++;
+	}
+	_putchar('\n');
+}
