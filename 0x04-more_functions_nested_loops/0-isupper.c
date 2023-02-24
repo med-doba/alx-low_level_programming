b361a9e388fcc0568b947b8cca9732768fb9b7fa
med-doba <mac@MacBook-Pro-de-mac-2.local>
2023-02-24 22:50:34 +0100

    a function that checks for uppercase character.

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isupper - checks for uppercase character.
+* @c: the character ascii
+* Return: 0 if c is lowercase and
+* 1 if it's uppercase
+*/
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}
